b08d601190d7a4b03662d58b0c9f4298915b8acb
Selvi Pebrina <98821404+selvipeb@users.noreply.github.com>
2022-04-26 10:55:37 +0400

    Add rubocop, gitignore and remove minitest files

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'capybara/rspec'
 
 RSpec.describe PagesController, type: :feature do
-
   describe 'GET #index' do
     it 'renders pages' do
       visit '/'

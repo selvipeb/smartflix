7f3c6b2f2ada63b97227e4c969f2d1998aba7e96
Selvi Pebrina <98821404+selvipeb@users.noreply.github.com>
2022-05-11 16:00:06 +0400

    Remove unused comments and update spec test

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# This is Films Controller
 class FilmsController < ApplicationController
   include FilmsHelper
   def index
--- app/helpers/films_helper.rb
+++ app/helpers/films_helper.rb
@@ -2,7 +2,6 @@
 
 require 'csv'
 
-# This is Films Helper
 module FilmsHelper
   FILE_PATH = 'lib/assets/netflix_titles.csv'
   def csv_data
